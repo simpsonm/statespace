f01811cc87c94bbfd40d96ed1a1756d0c1a6f6f1
Matthew Simpson <themattsimpson@gmail.com>
2013-06-27 01:37:06 -0500

    Initial testing of different priors for variance parameters

--- exper/vartest/varfun.r
+++ exper/vartest/varfun.r
@@ -2,6 +2,7 @@ library(dlm)
 library(MCMCpack)
 library(coda)
 
+
 ## function for creating the dlm model object to feed into MLE algorithm
 ## note: won't check inputs - make sure q is specified correctly
 ## note2: won't allow you to order the evolution variances if there are
@@ -40,22 +41,22 @@ mymod <- function(parm, other){
   return(mod)
 }
 
-## sample from the posterior of model 3b
-postsam <- function(n, data, prior, start){
-  ay <- prior[1]
-  by <- prior[2]
-  ath1 <- prior[3]
-  bth1 <- prior[4]
-  ath2 <- prior[5]
-  bth2 <- prior[6]
-  ath3 <- prior[7]
-  bth3 <- prior[8]
+## sample from the posterior of model 3b, IG prior
+postsamIG <- function(n, data, prior, start){
+  av <- prior[1]
+  bv <- prior[2]
+  am <- prior[3]
+  bm <- prior[4]
+  ab <- prior[5]
+  bb <- prior[6]
+  as <- prior[7]
+  bs <- prior[8]
   parm <- start
   T <- length(data)
-  Va <- ay+T/2
-  W1a <- ath1 + T/2
-  W2a <- ath2 + T/2
-  W3a <- ath3 + 3*T
+  Va <- av+T/2
+  W1a <- am + T/2
+  W2a <- ab + T/2
+  W3a <- as + 3*T
   out <- mcmc(matrix(0,ncol=4, nrow=n))
   colnames(out) <- c("V", "Wmu", "Wbeta", "Wseas")
   for(i in 1:n){
@@ -66,12 +67,198 @@ postsam <- function(n, data, prior, start){
     theta <- dlmBSample(filt)
     SSY <- sum((data - FF%*%t(theta[-1,]))^2)
     SSTH <- apply((t(theta[-1,]) - GG%*%t(theta[-(T+1),]))^2,1,sum)
-    Vp <- rgamma(1, Va, rate=by+0.5*SSY)  # note everything on precision scale here
-    W1p <- rgamma(1, W1a, rate=bth1 + 0.5*SSTH[1])
-    W2p <- rgamma(1, W2a, rate=bth2 + 0.5*SSTH[2])
-    W3p <- rgamma(1, W3a, rate=bth3 + 0.5*sum(SSTH[-c(1,2)]))
-    parm <- c(1/Vp, 1/W1p, 1/W2p, 1/W3p)
+    Vp <- rinvgamma(1, Va, bv+0.5*SSY)
+    W1p <- rinvgamma(1, W1a, bm + 0.5*SSTH[1])
+    W2p <- rinvgamma(1, W2a, bb + 0.5*SSTH[2])
+    W3p <- rinvgamma(1, W3a, bs + 0.5*sum(SSTH[-c(1,2)]))
+    parm <- c(Vp, W1p, W2p, W3p)
     out[i,] <- parm
   }
   return(out)
+}
+
+
+## sample from the posterior of model 3b, uniform prior, rejection steps
+postsamUR <- function(n, data, prior, start){
+  KV <- prior[1]
+  Km <- prior[2]
+  Kb <- prior[3]
+  Ks <- prior[4]
+  parm <- start
+  T <- length(data)
+  out <- mcmc(matrix(0,ncol=4, nrow=n))
+  colnames(out) <- c("V", "Wmu", "Wbeta", "Wseas")
+  for(i in 1:n){
+    print(i)
+    mod <- mymod(parm, c(2,24,3))
+    FF <- mod$FF
+    GG <- mod$GG
+    filt <- dlmFilter(data, mod)
+    theta <- dlmBSample(filt)
+    SSY <- sum((data - FF%*%t(theta[-1,]))^2)
+    SSTH <- apply((t(theta[-1,]) - GG%*%t(theta[-(T+1),]))^2,1,sum)
+
+    rej <- TRUE
+    while(rej){
+      Vp <- rinvgamma(1, Va, 0.5*SSY)
+      if(Vp<KV)
+          rej <- FALSE
+    }
+
+    rej <- TRUE
+    while(rej){
+      W1p <- rinvgamma(1, W1a, 0.5*SSTH[1])
+      if(W1p<Km)
+          rej <- FALSE
+    }
+
+    rej <- TRUE
+    while(rej){
+      W2p <- rinvgamma(1, W2a, 0.5*SSTH[2])
+      if(W2p<Kb)
+          rej <- FALSE
+    }
+
+    rej <- TRUE
+    while(rej){
+      W3p <- rinvgamma(1, W3a, 0.5*sum(SSTH[-c(1,2)]))
+      if(W3p<Ks)
+          rej <- FALSE
+    }
+    parm <- c(Vp, W1p, W2p, W3p)
+    out[i,] <- parm
+  }
+  return(out)
+}
+
+## sample from the posterior of model 3b, uniform prior, metropolis steps
+postsamUM <- function(n, data, prior, start){
+  KV <- prior[1]
+  Km <- prior[2]
+  Kb <- prior[3]
+  Ks <- prior[4]
+  parm <- start
+  T <- length(data)
+  out <- mcmc(matrix(0,ncol=4, nrow=n))
+  colnames(out) <- c("V", "Wmu", "Wbeta", "Wseas")
+  for(i in 1:n){
+    mod <- mymod(parm, c(2,24,3))
+    FF <- mod$FF
+    GG <- mod$GG
+    filt <- dlmFilter(data, mod)
+    theta <- dlmBSample(filt)
+    SSY <- sum((data - FF%*%t(theta[-1,]))^2)
+    SSTH <- apply((t(theta[-1,]) - GG%*%t(theta[-(T+1),]))^2,1,sum)
+
+    Vprop <- rinvgamma(1, Va, 0.5*SSY)
+    if(Vprop<KV)
+        Vp <- Vprop
+
+    W1prop <- rinvgamma(1, W1a, 0.5*SSTH[1])
+    if(W1prop<Km)
+        W1p <- W1prop
+
+    W2prop <- rinvgamma(1, W2a, 0.5*SSTH[2])
+    if(W2prop<Kb)
+        W2p <- W2prop
+
+    W3prop <- rinvgamma(1, W3a, 0.5*sum(SSTH[-c(1,2)]))
+    if(W3prop<Ks)
+        W3p <- W3prop
+
+    parm <- c(Vp, W1p, W2p, W3p)
+    out[i,] <- parm
+  }
+  return(out)
+}
+
+
+## sample from the posterior of model 3b, half-T prior
+postsamHT <- function(n, data, prior, start){
+  vv <- prior[1]
+  vm <- prior[2]
+  vb <- prior[3]
+  vs <- prior[4]
+  Av <- prior[5]
+  Am <- prior[6]
+  Ab <- prior[7]
+  As <- prior[8]
+
+  parm <- start
+  V <- parm[1]
+  Wm <- parm[2]
+  Wb <- parm[3]
+  Ws <- parm[4]
+
+  aV <- (T+vv)/2
+  aWm <- (T+vm)/2
+  aWb <- (T+vb)/2
+  aWs <- 3*T + vs/2
+
+  T <- length(data)
+  out <- mcmc(matrix(0,ncol=4, nrow=n))
+  colnames(out) <- c("V", "Wmu", "Wbeta", "Wseas")
+  for(i in 1:n){
+    print(c(i,round(parm,10)))
+    mod <- mymod(parm, c(2,24,3))
+    FF <- mod$FF
+    GG <- mod$GG
+    filt <- dlmFilter(data, mod)
+    theta <- dlmBSample(filt)
+    SSY <- sum((data - FF%*%t(theta[-1,]))^2)
+    SSTH <- apply((t(theta[-1,]) - GG%*%t(theta[-(T+1),]))^2,1,sum)
+
+    av <- rinvgamma(1, 1/2, 1/Av^2 + vv/V )
+    V <- rinvgamma(1, aV, vv/av + 0.5*SSY )
+
+    am <- rinvgamma(1, 1/2, 1/Am^2 + vm/Wm )
+    Wm <- rinvgamma(1, aWm, vm/am + 0.5*SSTH[1] )
+
+    ab <- rinvgamma(1, 1/2, 1/Ab^2 + vb/Wb )
+    Wb <- rinvgamma(1, aWb, vb/ab + 0.5*SSTH[2] )
+
+    as <- rinvgamma(1, 1/2, 1/As^2 + vs/Ws )
+    Ws <- rinvgamma(1, aWs, vs/as + 0.5*sum(SSTH[3:8]) )
+
+    parm <- c(V, Wm, Wb, Ws)
+    out[i,] <- parm
+  }
+  return(out)
+}
+
+
+## Simulates from a local level model
+llsim <- function(T, V, W, m0, C0){
+  out <- rep(0,T)
+  theta <- rnorm(1, m0, sqrt(C0))
+  for(t in 1:T){
+    theta <- theta + rnorm(1, 0, sd=sqrt(W))
+    out[t] <- theta + rnorm(1, 0, sd=sqrt(V))
+  }
+  return(out)
+}
+
+
+statesam <- function(n, start, dat, prior){
+  T <- length(dat)
+  V <- start[1]
+  W <- start[2]
+  out <- mcmc(matrix(0, nrow=n, ncol=2))
+  colnames(out) <- c("V","W")
+  vv <- prior[1]
+  vw <- prior[2]
+  Av <- prior[3]
+  Aw <- prior[4]
+  for(i in 1:n){
+    print(c(i,V,W))
+    mod <- dlmModPoly(order=1, dV=V, dW=W)
+    filt <- dlmFilter(dat, mod)
+    theta <- dlmBSample(filt)
+    av <- rinvgamma(1, 1/2, 1/(Av^2) + vv/V)
+    V <- rinvgamma(1,(vv + T)/2, vv/av + 0.5*sum((dat - theta[-1])^2))
+    aw <- rinvgamma(1, 1/2, 1/(Aw^2) + vw/W)
+    W <- rinvgamma(1, (vw + T)/2, vw/aw + 0.5*sum((theta[-1]-theta[-(T+1)])^2))
+    out[i,] <- c(V,W)
+  }
+    return(out)
 }
---
+++ exper/vartest/vartest.r
@@ -0,0 +1,113 @@
+source("varfun.r")
+
+dat2010 <- read.csv("../data/GYE_SoilMoistureTemperatureData_2010.csv")
+dat2011 <- read.csv("../data/GYE_SoilMoistureTemperatureData_2011.csv")
+
+## finds the 3 heated 25cm temperature 2011 time series and averages them
+idx <- grep("H.25cm.T", colnames(dat2011))
+datheat2011 <- dat2011[,idx]
+datMH11 <- apply(datheat2011,1,mean)[100:200]
+
+## Fit model 3b using MLE
+## note: parameter estimates are on log scale
+MLEest11 <- dlmMLE(datMH11, rep(0,4), mymodMLE, other=c(2,24,3))
+MLEstart <- exp(MLEest11$par) # my estimates don't replicate
+
+priorIG <- list(rep(1,8), rep(.1,8), rep(.01,8))
+priorU <- list(rep(10000,4), rep(10000000,4), rep(10000000000,4))
+priorHT <- list(c(rep(50,4), rep(1,4)), c(rep(1,4), rep(1,4)), c(rep(1,4), rep(10,4)))
+priortest <- c(rep(100,4), rep(1,4))
+
+test <- postsamHT(100, datMH11, priortest, MLEstart)
+
+chains <- 3
+n <- 100
+IGsam <- list()
+URsam <- list()
+UMsam <- list()
+HTsam <- list()
+starts <- list()
+starts[[1]] <- MLEstart*1/100
+starts[[2]] <- MLEstart
+starts[[3]] <- MLEstart*100
+
+for(j in 1:3){
+  IGsam[[j]] <- list()
+  URsam[[j]] <- list()
+  UMsam[[j]] <- list()
+  HTsam[[j]] <- list()
+  for(i in 1:chains){
+    print(c(i,j))
+    ##IGsam[[j]][[i]] <- postsamIG(n, datMH11, priorIG[[j]], starts[[i]])
+    print("IG finished")
+    ##URsam[[i]] <- postsamUR(n, datMH11, priorU[[3]], MLEstart)
+    print("UR finished")
+    ##UMsam[[j]][[i]] <- postsamUM(n, datMH11, priorU[[j]], starts[[i]])
+    print("UM finished")
+    HTsam[[j]][[i]] <- postsamHT(n, datMH11, priorHT[[j]], starts[[i]])
+    print("HT finished")
+  }
+}
+
+##plot ergodic means from multiple chains
+k <- 1
+par(mfrow=c(2,2))
+nams <- c("V","Wm", "Wb", "Ws")
+for(j in 1:3){
+  top <- max(c(cumsum(IGsam[[j]][[1]][,k])/1:n,cumsum(IGsam[[j]][[2]][,k])/1:n,cumsum(IGsam[[j]][[3]][,k])/1:n))
+  plot(ts(cumsum(IGsam[[j]][[1]][,k])/1:n), main=nams[k], ylim=c(0,top))
+  lines(ts(cumsum(IGsam[[j]][[2]][,k])/1:n), col="red")
+  lines(ts(cumsum(IGsam[[j]][[3]][,k])/1:n), col="blue")
+}
+
+k <- 1
+par(mfrow=c(2,2))
+nams <- c("V","Wm", "Wb", "Ws")
+for(j in 1:3){
+  top <- max(c(cumsum(UMsam[[j]][[1]][,k])/1:n,cumsum(UMsam[[j]][[2]][,k])/1:n,cumsum(UMsam[[j]][[3]][,k])/1:n))
+  plot(ts(cumsum(UMsam[[j]][[1]][,k])/1:n), main=nams[k], ylim=c(0,top))
+  lines(ts(cumsum(UMsam[[j]][[2]][,k])/1:n), col="red")
+  lines(ts(cumsum(UMsam[[j]][[3]][,k])/1:n), col="blue")
+}
+
+k <- 1
+par(mfrow=c(2,2))
+nams <- c("V","Wm", "Wb", "Ws")
+for(j in 1:3){
+  top <- max(c(cumsum(HTsam[[j]][[1]][,k])/1:n,cumsum(HTsam[[j]][[2]][,k])/1:n,cumsum(HTsam[[j]][[3]][,k])/1:n))
+  plot(ts(cumsum(HTsam[[j]][[1]][,k])/1:n), main=nams[k], ylim=c(0,top))
+  lines(ts(cumsum(HTsam[[j]][[2]][,k])/1:n), col="red")
+  lines(ts(cumsum(HTsam[[j]][[3]][,k])/1:n), col="blue")
+}
+
+par(mfrow=c(3,3))
+for(i in 1:3){
+  for(j in 1:3){
+    plot(ts(HTsam[[i]][[j]][,k]), main=paste(i,j))
+  }
+}
+
+  for(i in 1:chains){
+    ##lines(ts(cumsum(URsam[[i]][,j])/1:n), col="red")
+  }
+  for(i in 1:chains){
+    lines(ts(cumsum(UMsam[[i]][,j])/1:n), col="blue")
+  }
+  for(i in 1:chains){
+    lines(ts(cumsum(HTsam[[i]][,j])/1:n), col="green")
+  }
+  legend("topright", c("IG", "UR", "UM", "HT"), lty=c(1,1,1,1), col=c("black", "red", "blue", "green"))
+}
+
+plot(ts(HTsam[[1]][,2]))
+
+source("varfun.r")
+V <- 1
+W <- 1
+T <- 100
+dat <- llsim(T, V, W, 0, 1)
+start <- c(V,W)
+n <- 1000
+prior <- c(1,1,25,25)
+test <- mcmc(statesam(1000, start, dat, prior))
+plot(test)
