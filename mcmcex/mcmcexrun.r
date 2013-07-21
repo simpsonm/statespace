d06be9eae8810f64599cfbeefa512e2ccbead003
Matthew Simpson <themattsimpson@gmail.com>
2013-07-21 05:01:22 -0500

    Major reorganization preparing for new simulations. Saving old code because functions changed for new simulations.

--- mcmcex/mcmcexrun.r
+++ mcmcex/mcmcexrun.r
@@ -1,200 +1,40 @@
-## Simulates data from 36 local level models at 3 different lengths of the time series
-## then fits a local level model to each one using both the state sampler and the scaled
-## disturbance sampler. The samples and the autocorrelations from each chain are stored.
 
-## This takes about 18-24 hours to run
+## a wrapper for simulating from any of the interweaving/alternating
+## functions quickly in a loop
 
-## setup and data simulation
 source("mcmcexfun.r")
-n <- 2000
-burn <- 500
-Ts <- c(10,100,1000)
-LT <- length(Ts)
-a1 <- 5
-a2 <- 5
-Vs <- c(.01, .1, 1, 10, 100, 1000)
-LVW <- length(Vs)
-Ws <- Vs
+
+
 
 set.seed(152893627)
-simdata <- list()
+simdata <- data.frame(y=NULL, t=NULL, V=NULL, W=NULL, T=NULL)
 for(k in 1:LT){
   T <- Ts[k]
-  simdata[[k]] <- list()
   for(i in 1:LVW){
     V <- Vs[i]
-    simdata[[k]][[i]] <- list()
     for(j in 1:LVW){
       W <- Ws[j]
-      simdata[[k]][[i]][[j]] <- llsim(T, V, W, 0, 1)
+      y <- llsim(T, V, W, 0, 1)
+      newsim <- data.frame(y=y, t=1:length(y), V.T=V, W.T=W, T.T=T)
+      simdata <- merge(simdata, newsim, all=TRUE)
     }
   }
 }
 
-sVcors <- list()
-sWcors <- list()
-sTHcors <- list()
-ssam <- list()
-stimes <- list()
-
-
-## first do the state sampler
-for(k in 1:LT){
-  T <- Ts[k]
-  sVcors[[k]] <- matrix(0, LVW, LVW)
-  sWcors[[k]] <- matrix(0, LVW, LVW)
-  sTHcors[[k]] <- array(0, dim=c(LVW, LVW, T+1))
-  ssam[[k]] <- list()
-  stimes[[k]] <- list()
-  rownames(sVcors[[k]]) <- paste("V=", Vs, sep="")
-  rownames(sWcors[[k]]) <- paste("V=", Vs, sep="")
-  colnames(sVcors[[k]]) <- paste("W=", Ws, sep="")
-  colnames(sWcors[[k]]) <- paste("W=", Ws, sep="")
-  for(i in 1:LVW){
-    ssam[[k]][[i]] <- list()
-    stimes[[k]][[i]] <- list()
-    V <- Vs[i]
-    for(j in 1:LVW){
-      print(c(k,i,j))
-      W <- Ws[j]
-      b1 <- (a1-1)*V
-      b2 <- (a2-1)*W
-      start <- c(V,W)
-      dat <- simdata[[k]][[i]][[j]]
-      stateout <- statesam(n, start, dat, a1, a2, b1, b2, time=TRUE)
-      stimes[[k]][[i]][[j]] <- stateout[[2]]
-      state <- stateout[[1]][-c(1:burn),]
-      ssam[[k]][[i]][[j]] <- state
-      sTHcors[[k]][i,j,] <- diag(acf(state[,1:(T+1)], plot=FALSE, lag.max=1)$acf[2,,])
-      sVcors[[k]][i,j] <- acf(state[,T+2], plot=FALSE, lag.max=1)$acf[2]
-      sWcors[[k]][i,j] <- acf(state[,T+3], plot=FALSE, lag.max=1)$acf[2]
-    }
-  }
-}
-scors <- list(sTHcors, sVcors, sWcors)
-save(stimes, file="stimes.RData")
-save(scors, file="scors.RData")
-save(ssam, file="statsam.RData")
-rm(ssam)
+simdata1 <- data.frame(simdata, ch=1)
+simdata2 <- data.frame(simdata, ch=2)
+simdata3 <- data.frame(simdata, ch=3)
+simdat <- merge(merge(simdata1, simdata2, all=TRUE), simdata3, all=TRUE)
 
 
 
-## next do the scaled disturbance sampler
-set.seed(413013)
-dVcors <- list()
-dWcors <- list()
-dTHcors <- list()
-dsam <- list()
-dtimes <- list()
 
-for(k in 1:LT){
-  T <- Ts[k]
-  dVcors[[k]] <- matrix(0, LVW, LVW)
-  dWcors[[k]] <- matrix(0, LVW, LVW)
-  dTHcors[[k]] <- array(0, dim=c(LVW, LVW, T+1))
-  dsam[[k]] <- list()
-  dtimes[[k]] <- list()
-  rownames(dVcors[[k]]) <- paste("V=", Vs, sep="")
-  rownames(dWcors[[k]]) <- paste("V=", Vs, sep="")
-  colnames(dVcors[[k]]) <- paste("W=", Ws, sep="")
-  colnames(dWcors[[k]]) <- paste("W=", Ws, sep="")
-  for(i in 1:LVW){
-    dsam[[k]][[i]] <- list()
-    dtimes[[k]][[i]] <- list()
-    V <- Vs[i]
-    for(j in 1:LVW){
-      print(c(k,i,j))
-      W <- Ws[j]
-      b1 <- (a1-1)*V
-      b2 <- (a2-1)*W
-      start <- c(V,W)
-      dat <- simdata[[k]][[i]][[j]]
-      distout <- distsam(n, start, dat, a1, a2, b1, b2, time=TRUE)
-      dtimes[[k]][[i]][[j]] <- distout[[2]]
-      dist <- distout[[1]][-c(1:burn),]
-      dsam[[k]][[i]][[j]] <- dist
-      dTHcors[[k]][i,j,] <- diag(acf(dist[,1:(T+1)], plot=FALSE, lag.max=1)$acf[2,,])
-      dVcors[[k]][i,j] <- acf(dist[,T+2], plot=FALSE, lag.max=1)$acf[2]
-      dWcors[[k]][i,j] <- acf(dist[,T+3], plot=FALSE, lag.max=1)$acf[2]
-    }
-  }
-}
-
-dcors <- list(dTHcors, dVcors, dWcors)
-save(dcors, file="dcors.RData")
-save(dsam, file="distsam.RData")
-save(dtimes, file="dtimes.RData")
-rm(dsam)
-
-set.seed(413412)
-eVcors <- list()
-eWcors <- list()
-eTHcors <- list()
-esam <- list()
-etimes <- list()
-
-## finally do the scaled error sampler
-for(k in 1:LT){
-  T <- Ts[k]
-  eVcors[[k]] <- matrix(0, LVW, LVW)
-  eWcors[[k]] <- matrix(0, LVW, LVW)
-  eTHcors[[k]] <- array(0, dim=c(LVW, LVW, T+1))
-  esam[[k]] <- list()
-  etimes[[k]] <- list()
-  rownames(eVcors[[k]]) <- paste("V=", Vs, sep="")
-  rownames(eWcors[[k]]) <- paste("V=", Vs, sep="")
-  colnames(eVcors[[k]]) <- paste("W=", Ws, sep="")
-  colnames(eWcors[[k]]) <- paste("W=", Ws, sep="")
-  for(i in 1:LVW){
-    esam[[k]][[i]] <- list()
-    etimes[[k]][[i]] <- list()
-    V <- Vs[i]
-    for(j in 1:LVW){
-      print(c(k,i,j))
-      W <- Ws[j]
-      b1 <- (a1-1)*V
-      b2 <- (a2-1)*W
-      start <- c(V,W)
-      dat <- simdata[[k]][[i]][[j]]
-      errout <- errorsam(n, start, dat, a1, a2, b1, b2, time=TRUE)
-      etimes[[k]][[i]][[j]] <- errout[[2]]
-      err <- errout[[1]][-c(1:burn),]
-      esam[[k]][[i]][[j]] <- err
-      eTHcors[[k]][i,j,] <- diag(acf(err[,1:(T+1)], plot=FALSE, lag.max=1)$acf[2,,])
-      eVcors[[k]][i,j] <- acf(err[,T+2], plot=FALSE, lag.max=1)$acf[2]
-      eWcors[[k]][i,j] <- acf(err[,T+3], plot=FALSE, lag.max=1)$acf[2]
-    }
-  }
-}
-
-ecors <- list(eTHcors, eVcors, eWcors)
-save(ecors, file="ecors.RData")
-save(esam, file="errorsam.RData")
-save(etimes, file="etimes.Rdata")
-rm(esam)
+sams <- c("state", "dist", "error", "sdint", "seint", "deint", "triint", "sdalt", "sealt", "dealt", "trialt")
+sam <- ddply(simdata, .(V.T, W.T, T.T), samwrap, n=5, a1=5, a2=5,
+b1=5, b2=5, samp="state", ch=1)
 
 
 
-## code used to fix row and column names on correlations (not run)
-##load("ecors.RData")
-##load("scors.RData")
-##load("dcors.RData")
-##for(T in 1:3){
-##  rn <- colnames(ecors[[2]][[T]])
-##  cn <- rownames(ecors[[2]][[T]])
-##  colnames(ecors[[2]][[T]]) <- cn
-##  colnames(ecors[[1]][[T]]) <- cn
-##  rownames(ecors[[2]][[T]]) <- rn
-##  rownames(ecors[[1]][[T]]) <- rn
-##  colnames(scors[[2]][[T]]) <- cn
-##  colnames(scors[[1]][[T]]) <- cn
-##  rownames(scors[[2]][[T]]) <- rn
-##  rownames(scors[[1]][[T]]) <- rn
-##  colnames(dcors[[2]][[T]]) <- cn
-##  colnames(dcors[[1]][[T]]) <- cn
-##  rownames(dcors[[2]][[T]]) <- rn
-##  rownames(dcors[[1]][[T]]) <- rn
-##}
-##save(scors, file="scors.RData")
-##save(dcors, file="dcors.RData")
-##save(ecors, file="ecors.RData")
+
+
+
--- mcmcexOLD/mcmcexfun.r
+++ mcmcexOLD/mcmcexfun.r
@@ -4,123 +4,9 @@ library(dlm)
 library(coda)
 library(MCMCpack)
 library(ars)
-library(plyr)
 
-
-## simulates from a given sampler for each dataset and for multiple
-## chains, and returns summary info on the first chain.
-samsim <- function(simdata, n, burn, sampler, a1, a2){
-  parallel <- require(doMC)
-  sam <- ddply(simdata, .(V.T, W.T, T.T), samwrap, .parallel=parallel,
-               n=5, a1=a1, a2=a2, samp=sampler)
-  samnam <- paste(sampler, "SAM.Rdata", sep="")
-  write(sam, file=samnam)
-  out <- ddply(sam[sam$ch==1,], .(V.T, W.T, T.T), samsummary,
-               .parallel=parallel, dat=simdat[simdat$ch=1,], burn=burn,
-               sampler=sampler)
-  return(out)
-}
-
-## Finds autocorrelation and effective sample size info from a
-## sample from a given sampler
-samsummary <- function(sam, dat, burn, sampler){
-  theta0s <- sam[,grep("theta", colnames(sam))]
-  theta0s <- theta0s[-c(1:burn),1:(T.T+1)]
-  thetas <- theta0s[-c(1:burn),-1]
-  theta0 <- sam$theta0[-c(1:burn)]
-  V <- sam$V[-c(1:burn)]
-  W <- sam$W[-c(1:burn)]
-  V.T <- sam$V.T[1]
-  W.T <- sam$W.T[1]
-  T.T <- sam$T.T[1]
-  data <- dat$y[data$V.T=V.T & data$W.T=W.T & data$T.T=T.T, ]
-  time <- mean(sam$time[-c(1:burn)])
-  init <- data.frame(V=V.T, W=W.T, T=T.T, sampler=sampler, time=time)
-  gammas <- (theta0s[,-1] - theta0s[,-(T.T+1)])/sqrt(W)
-  colnames(gammas) <- paste("gamma", 1:T.T, sep="")
-  psis <- (matrix(data, ncol=1) - thetas)/sqrt(V)
-  colnames(psis) <- paste("psi", 1:T.T, sep="")
-  thetaAC <- apply(thetas, 2, corfun)
-  gammaAC <- apply(gammas, 2, corfun)
-  psiAC <- apply(psis, 2, corfun)
-  thetaES <- apply(thetas, 2, effectiveSize)
-  gammaES <- apply(gammas, 2, effectiveSize)
-  psiES <- apply(psis, 2, effectiveSize)
-  theta0.AC <- corfun(theta0)
-  theta1.AC <- thetaAC[1]
-  thetaT.AC <- thetaAC[T.T]
-  gamma1.AC <- gammaAC[1]
-  gammaT.AC <- gammaAC[T.T]
-  psi1.AC <- psiAC[1]
-  psiT.AC <- psiAC[T.T]
-  theta.AC <- thetaAC[which.max(abs(thetaAC))]
-  gamma.AC <- gammaAC[which.max(abs(gammaAC))]
-  psi.AC <- psiAC[which.max(abs(psiAC))]
-  theta0.ES <- effectiveSize(theta0)
-  theta1.ES <- thetaES[1]
-  thetaT.ES <- thetaES[T.T]
-  gamma1.ES <- gammaES[1]
-  gammaT.ES <- gammaES[T.T]
-  psi1.ES <- psiES[1]
-  psiT.ES <- psiES[T.T]
-  theta.ES <- thetaES[which.min(thetaES)]
-  gamma.ES <- gammaES[which.min(gammaES)]
-  psi.ES <- psiES[which.min(psiES)]
-  V.AC <- corfun(V)
-  W.AC <- corfun(W)
-  V.ES <- effectiveSize(V)
-  W.ES <- effectiveSize(W)
-  out <- cbind(init, V.AC, W.AC, theta0.AC, theta1.AC, thetaT.AC,
-               theta.AC, gamma1.AC, gammaT.AC, gamma.AC, psi1.AC, psiT.AC,
-               psi.AC, V.ES, W.ES, theta0.ES, theta1.ES, thetaT.ES,
-               theta.ES, gamma1.ES, gammaT.ES, gamma.ES, psi1.ES,
-               psiT.ES, psi.ES)
-  rownames(out) <- ""
-
-  return(out)
-}
-
-## function for finding the first order autocorrelation of a TS
-corfun <- function(x){
-  acf(x, lag.max=NULL, plot=FALSE)[[1]][2]
-}
-
-## Wrapper for quickly simulating from all samplers
-samwrap <- function(par, n, a1, a2, samp){
-  dat <- par$y[order(par$t)]
-  M <- c(1, 1/100, 100)
-  ch <- par$ch[1]
-  start <- c(par$V[1], par$W[1])*M[ch]
-  b1 <- (a1-1)*par$V[1]
-  b2 <- (a2-1)*par$W[1]
-  if(samp=="state")
-      out <- statesam(n, start, dat, a1, a2, b1, b2)
-  if(samp=="dist")
-      out <- distsam(n, start, dat, a1, a2, b1, b2)
-  if(samp=="error")
-      out <- errorsam(n, start, dat, a1, a2, b1, b2)
-  if(samp=="sdint")
-      out <- samwrapper(n, start, dat, a1, a2, b1, b2, TRUE, 1)
-  if(samp=="seint")
-      out <- samwrapper(n, start, dat, a1, a2, b1, b2, TRUE, 2)
-  if(samp=="deint")
-      out <- samwrapper(n, start, dat, a1, a2, b1, b2, TRUE, 3)
-  if(samp=="triint")
-      out <- samwrapper(n, start, dat, a1, a2, b1, b2, c(TRUE, TRUE), 4)
-  if(samp=="sdalt")
-      out <- samwrapper(n, start, dat, a1, a2, b1, b2, FALSE, 1)
-  if(samp=="sealt")
-      out <- samwrapper(n, start, dat, a1, a2, b1, b2, FALSE, 2)
-  if(samp=="dealt")
-      out <- samwrapper(n, start, dat, a1, a2, b1, b2, FALSE, 3)
-  if(samp=="trialt")
-      out <- samwrapper(n, start, dat, a1, a2, b1, b2, c(FALSE, FALSE), 4)
-  return(data.frame(out))
-}
-
-
-## A wrapper for quickly simulating from each of the interweaving/
-## alternating samplers.
+## a wrapper for simulating from any of the interweaving/alternating
+## functions quickly in a loop
 samwrapper <- function(n, start, dat, a1, a2, b1, b2, inter, samp){
   if(samp==1){
     out <- statedistinter(n, start, dat, a1, a2, b1, b2, inter)
@@ -149,39 +35,76 @@ llsim <- function(T, V, W, m0, C0){
 }
 
 ## state sampler: samples V and W conditional on states
-statesam <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0){
+statesam <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, time=FALSE){
   T <- length(dat)
   V <- start[1]
   W <- start[2]
-  out <- mcmc(matrix(0, nrow=n, ncol=T+4))
-  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W", "time")
+  out <- mcmc(matrix(0, nrow=n, ncol=T+3))
+  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W")
 
+  if(time){
+    times <- matrix(0, nrow=n, ncol=3)
+    colnames(times) <- c("states", "V", "W")
+    itime <- rep(0,4)
+  }
 
   for(i in 1:n){
-    time <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
+    if(time){
+      itime[1] <- proc.time()[3]
+    }
+
     mod <- dlmModPoly(order=1, dV=V, dW=W)
     filt <- dlmFilter(dat, mod)
     theta <- dlmBSample(filt)
+
+    if(time){
+      itime[2] <- proc.time()[3]
+    }
+
     V <- rinvgamma(1, a1 + T/2, b1 + sum((dat-theta[-1])^2)/2)
+
+    if(time){
+      itime[3] <- proc.time()[3]
+    }
+
     W <- rinvgamma(1, a2 + T/2, b2 + sum((theta[-1]-theta[-(T+1)])^2)/2)
-    time2 <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
-    time <- time2-time
-    out[i,] <- c(theta,V,W, time)
+
+    if(time){
+      itime[4] <- proc.time()[3]
+      times[i,] <- itime[-1] - itime[-4]
+    }
+
+    out[i,] <- c(theta,V,W)
   }
-  return(out)
+
+  if(time){
+    return(list(out, times))
+  }
+  else{
+    return(out)
+  }
 }
 
 ## scaled error sampler: samples V and W conditional on the scaled observation
 ## errors (plus the initial state, theta_0)
-errorsam <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0){
+errorsam <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, time=FALSE){
   T <- length(dat)
   V <- start[1]
   W <- start[2]
-  out <- mcmc(matrix(0, nrow=n, ncol=T+4))
-  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W", "time")
+  out <- mcmc(matrix(0, nrow=n, ncol=T+3))
+  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W")
+
+  if(time){
+    times <- matrix(0, nrow=n, ncol=4)
+    itime <- rep(0,5)
+    rejsam <- rep(0,n)
+  }
 
   for(i in 1:n){
-    time <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
+    if(time){
+      itime[1] <- proc.time()[1]
+    }
+
     mod <- dlmModPoly(order=1, dV=V, dW=W)
     filt <- dlmFilter(dat, mod)
     theta <- dlmBSample(filt)
@@ -189,9 +112,19 @@ errorsam <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0){
     A[1,1] <- 1
     ytild <- c(0, dat/sqrt(V))
     psi <- ytild + A%*%theta
+
+    if(time){
+      itime[2] <- proc.time()[1]
+    }
+
     Wa <- a2 + T/2
     Wb <- b2 + sum( (theta[-1] - theta[-(T+1)])^2 )/2
     W <- rinvgamma(1, Wa, Wb)
+
+    if(time){
+      itime[3] <- proc.time()[1]
+    }
+
     psi0 <- psi[1]
     psiLT <- c(0,psi[-1])
     Lpsi <- psiLT[-1] - psiLT[-(T+1)]
@@ -201,9 +134,15 @@ errorsam <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0){
     b <- sum(Lpsi*Ly)/W
     mn <- optimize(logpiVW, c(0,10^10), maximum=TRUE, a=a, b=b, a12=a1, b12=b1)$maximum
     if(logcon(b, a1, b1)){
+      if(time){
+        rejsam[i] <- "ARS"
+      }
       V <- ars(n=1, logpiVW, logpiVWprime, x=c(mn/2, mn, mn*2), lb=TRUE, xlb=0, a=a, b=b, a12=a1, b12=b1)
     }
     else{
+      if(time){
+        rejsam[i] <- "TPROP"
+      }
       propvar <- - 1 /( (a1 + 1)*mn^(-2) - b*mn^(-3/2)/4 - 2*b1*mn^(-3) )
       d <- optimize(propM, c(1,10^10), maximum=FALSE,  a=a, b=b, a12=a1, b12=b1, mn=mn, propvar=propvar)
       M <- d$objective
@@ -222,33 +161,56 @@ errorsam <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0){
       }
     }
 
+    if(time){
+      itime[4] <- proc.time()[1]
+    }
+
     A <- diag(-1/sqrt(V), T+1 )
     A[1,1] <- 1
     ytild <- c(0, dat/sqrt(V))
     theta <- solve(A)%*%(psi - ytild)
-    time2 <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
-    time <- time2-time
-    out[i,] <- c(theta,V,W,time)
 
+    if(time){
+      itime[5] <- proc.time()[1]
+      times[i,] <- c(itime[-1] - itime[-4])
+    }
+
+    out[i,] <- c(theta,V,W)
   }
-  return(out)
+
+  if(time){
+    timeout <- data.frame(states=times[,1] + times[,4], V=times[,3], W=times[,2], rejsam=rejsam)
+    return(list(out, timeout))
+  }
+  else{
+    return(out)
+  }
 }
 
 
 ## scaled disturbance sampler: samples V and W conditional on the scaled
 ## system disturbances (plus the initial state, theta_0)
-distsam <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0){
+distsam <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, time=FALSE){
   T <- length(dat)
   V <- start[1]
   W <- start[2]
-  out <- mcmc(matrix(0, nrow=n, ncol=T+4))
-  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W", "time")
+  out <- mcmc(matrix(0, nrow=n, ncol=T+3))
+  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W")
   B <- diag(1,T+1,T+1)
   C <- diag(-1,T,T)
   B <- rbind(rep(0,T+1),cbind(C,rep(0,T))) + B
 
+  if(time){
+    times <- matrix(0, nrow=n, ncol=4)
+    itime <- rep(0,5)
+    rejsam <- rep(0,n)
+  }
+
   for(i in 1:n){
-    time <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
+    if(time){
+      itime[1] <- proc.time()[1]
+    }
+
     mod <- dlmModPoly(order=1, dV=V, dW=W)
     filt <- dlmFilter(dat, mod)
     theta <- dlmBSample(filt)
@@ -257,16 +219,32 @@ distsam <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0){
     gam <- A%*%theta
     cgam <- cumsum(gam[-1])
     gam0 <- gam[1]
+
+    if(time){
+      itime[2] <- proc.time()[1]
+    }
+
     Va <- a1 + T/2
     Vb <- b1 + sum( (dat - theta[-1])^2 )/2
     V <- rinvgamma(1, Va, Vb)
+
+    if(time){
+      itime[3] <- proc.time()[1]
+    }
+
     a <- sum( cgam^2 )/2/V
     b <- sum( (dat - gam0) * cgam ) /V
     mn <- optimize(logpiVW, c(0,10^10), maximum=TRUE, a=a, b=b, a12=a2, b12=b2)$maximum
     if(logcon(b, a2, b2)){
+      if(time){
+        rejsam[i] <- "ARS"
+      }
       W <- ars(n=1, logpiVW, logpiVWprime, x=c(mn/2, mn, mn*2), lb=TRUE, xlb=0, a=a, b=b, a12=a2, b12=b2)
     }
     else{
+      if(time){
+        rejsam[i] <- "TPROP"
+      }
       propvar <- - 1 /( (a2 + 1)*mn^(-2) - b*mn^(-3/2)/4 - 2*b2*mn^(-3) )
       d <- optimize(propM, c(1,10^10), maximum=FALSE,  a=a, b=b, a12=a2, b12=b2, mn=mn, propvar=propvar)
       M <- d$objective
@@ -284,14 +262,30 @@ distsam <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0){
         }
       }
     }
+
+    if(time){
+      itime[4] <- proc.time()[1]
+    }
+
     A <- B/sqrt(W)
     A[1,1] <- 1
     theta <- solve(A)%*%gam
-    time2 <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
-    time <- time2-time
-    out[i,] <- c(theta,V,W, time)
+
+    if(time){
+      itime[5] <- proc.time()[1]
+      times[i,] <- c(itime[-1] - itime[-4])
+    }
+
+    out[i,] <- c(theta,V,W)
   }
-  return(out)
+
+  if(time){
+    timeout <- data.frame(states=times[,1] + times[,4], V=times[,2], W=times[,3], rejsam=rejsam)
+    return(list(out, timeout))
+  }
+  else{
+    return(out)
+  }
 }
 
 ## state + dist interveaving or alternating sampler
@@ -299,14 +293,13 @@ statedistinter <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, inter=TRUE){
   T <- length(dat)
   V <- start[1]
   W <- start[2]
-  out <- mcmc(matrix(0, nrow=n, ncol=T+4))
-  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W", "time")
+  out <- mcmc(matrix(0, nrow=n, ncol=T+3))
+  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W")
   B <- diag(1,T+1,T+1)
   C <- diag(-1,T,T)
   B <- rbind(rep(0,T+1),cbind(C,rep(0,T))) + B
 
   for(i in 1:n){
-    time <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
     ## sample states
     mod <- dlmModPoly(order=1, dV=V, dW=W)
     filt <- dlmFilter(dat, mod)
@@ -357,11 +350,9 @@ statedistinter <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, inter=TRUE){
     A <- B/sqrt(W)
     A[1,1] <- 1
     theta <- solve(A)%*%gam
-    time2 <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
-    time <- time2-time
-    out[i,] <- c(theta,V,W,time)
+    out[i,] <- c(theta,V,W)
   }
-  return(out)
+    return(out)
 }
 
 ## state + error interveaving/alternating sampler
@@ -369,10 +360,9 @@ stateerrorinter <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, inter=TRUE){
   T <- length(dat)
   V <- start[1]
   W <- start[2]
-  out <- mcmc(matrix(0, nrow=n, ncol=T+4))
-  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W","time")
+  out <- mcmc(matrix(0, nrow=n, ncol=T+3))
+  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W")
   for(i in 1:n){
-    time <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
     ## sample states
     mod <- dlmModPoly(order=1, dV=V, dW=W)
     filt <- dlmFilter(dat, mod)
@@ -428,11 +418,9 @@ stateerrorinter <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, inter=TRUE){
     A[1,1] <- 1
     ytild <- c(0, dat/sqrt(V))
     theta <- solve(A)%*%(psi - ytild)
-    time2 <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
-    time <- time2-time
-    out[i,] <- c(theta,V,W,time)
+    out[i,] <- c(theta,V,W)
   }
-  return(out)
+    return(out)
 }
 
 ## dist + error interveaving/alternating sampler
@@ -440,13 +428,13 @@ disterrorinter <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, inter=TRUE){
   T <- length(dat)
   V <- start[1]
   W <- start[2]
-  out <- mcmc(matrix(0, nrow=n, ncol=T+4))
-  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W", "time")
+  out <- mcmc(matrix(0, nrow=n, ncol=T+3))
+  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W")
   B <- diag(1,T+1,T+1)
   C <- diag(-1,T,T)
   B <- rbind(rep(0,T+1),cbind(C,rep(0,T))) + B
+
   for(i in 1:n){
-    time <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
     ## sample states and convert to disturbances
     mod <- dlmModPoly(order=1, dV=V, dW=W)
     filt <- dlmFilter(dat, mod)
@@ -460,6 +448,7 @@ disterrorinter <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, inter=TRUE){
     Va <- a1 + T/2
     Vb <- b1 + sum( (dat - theta[-1])^2 )/2
     V <- rinvgamma(1, Va, Vb)
+
     a <- sum( cgam^2 )/2/V
     b <- sum( (dat - gam0) * cgam ) /V
     mn <- optimize(logpiVW, c(0,10^10), maximum=TRUE, a=a, b=b, a12=a2, b12=b2)$maximum
@@ -538,9 +527,7 @@ disterrorinter <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, inter=TRUE){
     A[1,1] <- 1
     ytild <- c(0, dat/sqrt(V))
     theta <- solve(Ap)%*%(psi - ytild)
-    time2 <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
-    time <- time2-time
-    out[i,] <- c(theta,V,W,time)
+    out[i,] <- c(theta,V,W)
   }
     return(out)
 }
@@ -550,14 +537,13 @@ tripleinter <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, inter=c(TRUE, TRU
   T <- length(dat)
   V <- start[1]
   W <- start[2]
-  out <- mcmc(matrix(0, nrow=n, ncol=T+4))
-  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W","time")
+  out <- mcmc(matrix(0, nrow=n, ncol=T+3))
+  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W")
   B <- diag(1,T+1,T+1)
   C <- diag(-1,T,T)
   B <- rbind(rep(0,T+1),cbind(C,rep(0,T))) + B
 
   for(i in 1:n){
-    time <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
     ## sample states
     mod <- dlmModPoly(order=1, dV=V, dW=W)
     filt <- dlmFilter(dat, mod)
@@ -659,9 +645,7 @@ tripleinter <- function(n, start, dat, a1=0, a2=0, b1=0, b2=0, inter=c(TRUE, TRU
     A[1,1] <- 1
     ytild <- c(0, dat/sqrt(V))
     theta <- solve(Ap)%*%(psi - ytild)
-    time2 <- sum(as.numeric(unlist(strsplit(format(Sys.time(), "%M:%OS3"), ":")))*c(60, 1))
-    time <- time2-time
-    out[i,] <- c(theta,V,W,time)
+    out[i,] <- c(theta,V,W)
   }
     return(out)
 }
@@ -715,7 +699,4 @@ logpirej <- function(VW, a, b, a12, b12, mn, propvar, df){
   out <- logpiVW(VW, a, b, a12, b12) - dtprop(VW, mn, propvar, df)
   return(out)
 }
-
-
-
 
