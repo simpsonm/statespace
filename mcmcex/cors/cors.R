373b8411794c8c81f0ea2afb30db31f95500e133
simpsonm <themattsimpson@gmail.com>
2013-11-05 12:42:39 -0600

    Fixed another bug

--- mcmcex/cors/cors.R
+++ mcmcex/cors/cors.R
@@ -14,32 +14,32 @@ itsim <- function(dat, y){
   W.T <- dat$W.T[1]
   T.T <- dat$T.T[1]
   V <- dat$V[1]
-  W <- dat$V[1]
+  W <- dat$W[1]
   yt <- y$y[y$V.T==V.T & y$W.T==W.T & y$T.T==T.T]
   mod <- dlmModPoly(order=1, dV=V, dW=W)
   filt <- dlmFilter(yt, mod)
   theta <- dlmBSample(filt)
-  v2 <- sum( (yt - theta[-1])^2 )
-  w2 <- sum( (theta[-1] - theta[-(T.T+1)])^2 )
+  bv <- sum( (yt - theta[-1])^2 )
+  bw <- sum( (theta[-1] - theta[-(T.T+1)])^2 )
   out <- dat
-  out$v2 <- v2
-  out$w2 <- w2
+  out$bv <- bv
+  out$bw <- bw
   return(out)
 }
 
-samcor <- ddply(.data=samshort, .variables=.(iter), .fun=itsim, y=simdata)
+samcor <- ddply(.data=samshort2, .variables=.(iter), .fun=itsim, y=simdata)
 rm(samshort)
 
 newcors <- function(samcor){
-  Vv2 <- cor(samcor$V, samcor$v2)
-  Wv2 <- cor(samcor$W, samcor$v2)
-  Vw2 <- cor(samcor$V, samcor$w2)
-  Ww2 <- cor(samcor$W, samcor$w2)
+  Vbv <- cor(samcor$V, samcor$bv)
+  Wbv <- cor(samcor$W, samcor$bv)
+  Vbw <- cor(samcor$V, samcor$bw)
+  Wbw <- cor(samcor$W, samcor$bw)
   out <- data.frame(V=samcor$V.T[1], W=samcor$W.T[1], T=samcor$T.T[1])
-  out$Vv2 <- Vv2
-  out$Wv2 <- Wv2
-  out$Vw2 <- Vw2
-  out$Ww2 <- Ww2
+  out$Vbv <- Vbv
+  out$Wbv <- Wbv
+  out$Vbw <- Vbw
+  out$Wbw <- Wbw
   return(out)
 }
 
