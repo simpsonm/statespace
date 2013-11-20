5d48fe2901f4cd2fd101fb6d9cdb42c0a3104154
simpsonm <themattsimpson@gmail.com>
2013-11-20 01:40:02 -0600

    Added functions for simulating V, W jointly using a metropolis step w/ normal proposal

---
+++ mcmcex/met/metropsim.R
@@ -0,0 +1,29 @@
+source("../mcmcexfun.R")
+source("metropfun.R")
+set.seed(152893627)
+T <- c(10, 100, 1000)
+V <- 10^(c(0:10)/2-2)
+W <- V
+
+T <- 10
+V <- 10^c(0,1)
+W <- V
+
+simgrid <- expand.grid(V.T=V, W.T=W, T.T=T)
+simdata <- ddply(simgrid, .(V.T, W.T, T.T), lldsim, m0=0, C0=1)
+simdata$av <- 5
+simdata$aw <- 5
+simdata$bv <- (simdata$av-1)*simdata$V.T
+simdata$bw <- (simdata$aw-1)*simdata$W.T
+sams <- c("dist", "error")
+samplers <- data.frame(sams=rep(1,length(sams)))
+samplers$sampler <- sams
+n <- 3000
+burn <- 500
+parallel <- require(doMC, quietly=TRUE)
+if(parallel){
+  registerDoMC(4)
+}
+system.time(metout <- metfullsim(samplers, simdata, n, burn, parallel))
+save(metout, file="samout.RData")
+
