8462db050319d2c5aad96ca66872d7ab88f573e6
simpsonm <themattsimpson@gmail.com>
2014-03-10 17:16:07 -0500

    Got the wrongly scaled rejection step working nicely - adaptive for the log variances most of the time

--- mcmcex/DArej.R
+++ mcmcex/DArej.R
@@ -1,6 +1,6 @@
 ## compute M in logtarget - logprop <= M
-propMda <- function(df, a, b, c, avw, mn, propvar){
-  M <- optimize(logpirejda, c(0,10^10), maximum=TRUE, a, b, c, avw, mn, propvar, df)
+propMda <- function(df, a, b, cc, avw, mn, propvar){
+  M <- optimize(logpirejda, c(-10^2,10^2), maximum=TRUE, a, b, cc, avw, mn, propvar, df)
   return(M$objective)
 }
 
@@ -21,20 +21,27 @@ rtpropda <- function(n, mn, var, df){
 }
 
 ## log of the conditional posterior density of W (V) given V (W), gamma (psi), data
-logpiVWda <- function(VW, a, b, c, avw){
-  out <-  - (avw + 1)*log(VW) - a/VW + b/sqrt(VW) - c*VW
+logpiVWda <- function(VW, a, b, cc, avw){
+  out <-  - avw*VW - a*exp(-VW) + b*exp(-VW/2) - cc*exp(VW)
   return(out)
 }
 
 ## first derivative of log of the conditional posterior of W (V)
-logpiVWprimeda <- function(VW, a, b, c, avw){
-  out <-  - (avw + 1)/VW +a/(VW^2) -b/(2*VW^(3/2)) - c
+logpiVWprimeda <- function(VW, a, b, cc, avw){
+  out <-  - avw + a*exp(-VW) - (b/2)*exp(-VW/2) - cc*exp(VW)
+  return(out)
+}
+
+logconda <- function(a, b, cc){
+  out <- (b <= 4*cc)
+  if(!out)
+      out <- (-a -b/16 + (b/4 - cc)*(b/cc/16)^(1/3) < 0 )
   return(out)
 }
 
 ## difference between log conditional posterior of W (V) and the proposal density
-logpirejda <- function(VW, a, b, c, avw, mn, propvar, df){
-  out <- logpiVWda(VW, a, b, c, avw) - dtpropda(VW, mn, propvar, df)
+logpirejda <- function(VW, a, b, cc, avw, mn, propvar, df){
+  out <- logpiVWda(VW, a, b, cc, avw) - dtpropda(VW, mn, propvar, df)
   return(out)
 }
 
@@ -79,39 +86,46 @@ Wpsiiterda <- function(dat, psi, V, aw, bw){
 }
 
 
-
 VWrejda <- function(a, b, cc, avw){
-  mn <- optimize(logpiVWda, c(0,10^10), maximum=TRUE, a=a, b=b, c=cc, avw=avw)$maximum
-  propvar <- - 1 /( (avw + 1)*mn^(-2) - 2*a*mn^(-3) + (3/4)*b*mn^(-5/2) )
-  d <- optimize(propMda, c(1,10^10), maximum=FALSE,  a=a, b=b, c=cc, avw=avw, 
-                mn=mn, propvar=propvar)
-  M <- d$objective
-  df <- d$minimum
-  rej <- TRUE
-  rejit <- 1
-  while(rej){
-    VW <- rtpropda(1, mn, propvar, df)
-    if(VW>0){
+  mn <- uniroot(logpiVWprimeda, c(-10^2, 10^2), a=a, b=b, cc=cc, avw=avw)$root
+  adrej <- logconda(a, b, cc)
+  adrej1 <- adrej
+  if(adrej){
+    try(VW <- ars(n=1, logpiVWda, logpiVWprimeda, x=c(mn-5*mn*2, mn, 5*mn*2),
+                  a=a, b=b, cc=cc, avw=avw))
+    if(VW==0){
+      adrej <- FALSE
+    }
+  }
+  if(!adrej){ 
+    propvar <- - 1 /( -a*exp(-mn) +(b/4)*exp(-mn/2) - cc*exp(mn) )
+    d <- optimize(propMda, c(1,10^3), maximum=FALSE,  a=a, b=b, cc=cc, avw=avw, 
+                  mn=mn, propvar=propvar)
+    M <- d$objective
+    df <- d$minimum
+    rej <- TRUE
+    rejit <- 1
+    while(rej){
+      VW <- rtpropda(1, mn, propvar, df)
       R <- logpirejda(VW, a, b, cc, avw, mn, propvar, df) - M
       u <- runif(1,0,1)
       if(log(u)<R){
         rej <- FALSE
       }
-    }
-    rejit <- rejit + 1
-    if(rejit%%1000000==0){
-      print(paste("Rejection sampler appears to be stuck for VW, ",
-                  rejit, " iterations", sep=""))
-      print(paste("Last value of W in the chain: ", VW, sep=""))
-      print(paste("Values of a b and c:", a, b, cc, sep="  "))
-      print(paste("T = ", T, sep=""))
-      print(paste("Prior value of alphaVW:", avw, sep="  "))
-      print(paste("Attempted df: ", df, sep=""))
-      print(paste("Attempted mean and variance:", mn, propvar, sep="  "))
+      rejit <- rejit + 1
+      if(rejit%%1000000==0){
+        print(paste("Rejection sampler appears to be stuck for VW, ",
+                    rejit, " iterations", sep=""))
+        print(paste("Last value of W in the chain: ", VW, sep=""))
+        print(paste("Values of a b and c:", a, b, cc, sep="  "))
+        print(paste("T = ", T, sep=""))
+        print(paste("Prior value of alphaVW:", avw, sep="  "))
+        print(paste("Attempted df: ", df, sep=""))
+        print(paste("Attempted mean and variance:", mn, propvar, sep="  "))
+      }
     }
   }
-
-  return(VW)
+  return(c(adrej1, adrej, exp(VW)))
 }
 
 ## scaled error sampler: samples V and W conditional on the scaled observation
@@ -121,19 +135,18 @@ errorsamda <- function(n, start, dat, av=0, aw=0, bv=0, bw=0){
   V <- start[1]
   W <- start[2]
   out <- mcmc(matrix(0, nrow=n, ncol=T+8))
-  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W","logconV",
+  colnames(out) <- c(paste("theta",0:T,sep=""),"V","W","logcon",
                      "adrejV", "logconW", "adrejW", "kernel")
   for(i in 1:n){
     theta <- thetaiter(dat, V, W)
-    psit <- psitrans(dat, theta, V)
-    psi <- psit*sqrt(V)/sqrt(W)
-    psi[1] <- psit[1]
+    psit <- (dat - theta[-1])/sqrt(W)
+    psi <- c(theta[1], psit)
     V <- Vpsiiterda(dat, psi, W, av, bv)
-    W <- Wpsiiterda(dat, psi, V,  aw, bw)
-    psit <- psi*sqrt(W)/sqrt(V)
-    psit[1] <- psi[1]
-    theta <- thetapsitrans(dat, psi, V)
-    out[i,] <- c(theta,V,W,c(NA,NA),c(NA,NA), NA)
+    Wout <- Wpsiiterda(dat, psi, V,  aw, bw)
+    W <- Wout[3]
+    thetat <- dat - sqrt(W)*psi[-1]
+    theta <- c(psi[1], thetat)
+    out[i,] <- c(theta,V,W,c(NA,NA),Wout[1:2], NA)
   }
   return(out)
 }
@@ -150,15 +163,14 @@ distsamda <- function(n, start, dat, av=0, aw=0, bv=0, bw=0){
                      "logconV", "adrejV", "logconW", "adrejW", "kernel")
   for(i in 1:n){
     theta <- thetaiter(dat, V, W)
-    gamt <- gamtrans(theta, W)
-    gam <- gamt*sqrt(W)/sqrt(V)
-    gam[1] <- gamt[1]
-    V <- Vgamiterda(dat, gam, W, av, bv)
+    gamt <- (theta[-1] - theta[-(T+1)])/sqrt(V)
+    gam <- c(theta[1], gamt)
+    Vout <- Vgamiterda(dat, gam, W, av, bv)
+    V <- Vout[3]
     W <- Wgamiterda(dat, gam, V, aw, bw)
-    gamt <- gam*sqrt(V)/sqrt(W)
-    gamt[1] <- gam[1]
-    theta <- thetagamtrans(gam, W)
-    out[i,] <- c(theta,V,W,c(NA,NA),c(NA,NA), NA)
+    thetat <- gam[1] + cumsum(gam[-1])*sqrt(V)
+    theta <- c(gam[1], thetat)
+    out[i,] <- c(theta,V,W,Vout[1:2],c(NA,NA), NA)
   }
   return(out)
 }
